e2afe4dbb1eb374a89a780db2d47736a2051a3b5
Rogojan Raul <rogojan_raul@yahoo.com>
2024-11-07 15:24:05 +0200

    first commit - client side plus routes

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from 'next';
+import './globals.css';
+import { poppins } from '@/fonts';
+
+export const metadata: Metadata = {
+  title: 'Portfolio',
+  description: 'Personal Portfolio',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={`${poppins.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ client/src/routes/index.tsx
@@ -0,0 +1,50 @@
+export const routes = {
+  home: '/',
+  login: '/login',
+  signup: '/signup',
+  about: '/about',
+  profile: '/profile',
+};
+
+export const routesConfig = [
+  {
+    title: 'Home',
+    url: routes.home,
+    showInMenu: true,
+    showInFooter: false,
+    isProtected: false,
+    openInNewTab: false,
+  },
+  {
+    title: 'LogIn',
+    url: routes.login,
+    showInMenu: true,
+    showInFooter: false,
+    isProtected: false,
+    openInNewTab: false,
+  },
+  {
+    title: 'SignUp',
+    url: routes.signup,
+    showInMenu: true,
+    showInFooter: false,
+    isProtected: false,
+    openInNewTab: false,
+  },
+  {
+    title: 'Profile',
+    url: routes.profile,
+    showInMenu: true,
+    showInFooter: false,
+    isProtected: false,
+    openInNewTab: false,
+  },
+  {
+    title: 'About',
+    url: routes.about,
+    showInMenu: true,
+    showInFooter: false,
+    isProtected: false,
+    openInNewTab: false,
+  },
+];
