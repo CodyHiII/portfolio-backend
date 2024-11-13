5843173fd84ac9a7895cd91604d8d0f58004d700
Rogojan Raul <rogojan_raul@yahoo.com>
2024-11-13 18:21:42 +0200

    connecting to mongo db

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.NEXT_PUBLIC_MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
-const port = 5500;
-
-app.listen(port, () => {
-  console.log('Server over here on port:', port);
+app.listen(process.env.NEXT_PUBLIC_SERVER_PORT, () => {
+  console.log('Server started on:', process.env.NEXT_PUBLIC_SERVER_PORT);
 });
